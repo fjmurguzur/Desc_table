5eed603f9727712c0eb625e3b77232e2177a09ec
fjmurguzur <francisco.j.murguzur@uit.no>
2023-02-22 09:29:41 +0100

    Add files via upload

---
+++ Descriptive_table.R
@@ -0,0 +1,12 @@
+descriptive.table <- function(x,name=NA){
+
+  y<-x[complete.cases(x)]
+    
+  result.table <- cbind.data.frame(Variable=name,Mean=round(mean(y),digits=1),Min=round(min(y),digits=1),Max=round(max(y),digits=1),
+                             SD=round(sd(y),digits=1),CV=round(100*(sd(y)/mean(y)),digits=1),Total=length(y),check.names=F)
+      
+
+    write.table(result.table,"clipboard",sep="\t",row.names=F)
+    return(result.table)
+ }
+
