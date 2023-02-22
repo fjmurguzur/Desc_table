a141592e9780684429955d9ed54c483d1347afdb
fjmurguzur <francisco.j.murguzur@uit.no>
2023-02-22 09:31:44 +0100

    .

--- Descriptive_table.R
+++ Descriptive_table.R
@@ -3,7 +3,7 @@ descriptive.table <- function(x,name=NA){
   y<-x[complete.cases(x)]
     
   result.table <- cbind.data.frame(Variable=name,Mean=round(mean(y),digits=1),Min=round(min(y),digits=1),Max=round(max(y),digits=1),
-                             SD=round(sd(y),digits=1),CV=round(100*(sd(y)/mean(y)),digits=1),Total=length(y),check.names=F)
+                             SD=round(sd(y),digits=1),CV=round(100*(sd(y)/mean(y)),digits=1),Total=length(y))
       
 
     write.table(result.table,"clipboard",sep="\t",row.names=F)
